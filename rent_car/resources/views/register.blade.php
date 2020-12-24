f74e341f7187e7a9773489c68a6cdf85172f23d1
Muhammad Iqbal <miqbaal1605@gmail.com>
2020-12-24 23:24:06 +0700

    edit

--- rent_car/resources/views/register.blade.php
+++ rent_car/resources/views/register.blade.php
@@ -15,6 +15,7 @@
     <!-- Icons font CSS-->
     <link href="assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
     <link href="assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+
     <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 
@@ -23,7 +24,7 @@
     <link href="assets/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="assets/css/main.css" rel="stylesheet" media="all">
+    <link href="assets/css/register.css" rel="stylesheet" media="all">
 </head>
 
 <body>
@@ -86,7 +87,7 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="asset/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
     <!-- Vendor JS-->
     <script src="assets/vendor/select2/select2.min.js"></script>
     <script src="assets/vendor/datepicker/moment.min.js"></script>
